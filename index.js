7487ad79e733fd3346af5742e02f43894a9b1696
zagnit <riftersound@gmail.com>
2018-03-01 21:08:27 +0200

    First commit

--- index.js
+++ index.js
@@ -6,5 +6,69 @@ const
 
 (async() => {
   //const net =require('./net');
-  console.log("index");
+  let {global, rides}=require('./parsed/a_example.json');
+  let autos = [];
+  for (let index = 0; index < global.vehicles; index++) {
+    autos.push({
+      x: 0,
+      y: 0,
+      ride: -1,
+      finishedRides: [],
+      availableRides: [],
+    })
+  }
+
+  for (let i in rides) {
+    let ride = rides[i];
+    rides[i].distance = getDistance(ride.xStart, ride.yStart, ride.xFinish, ride.yFinish);
+  }
+
+  let sortedRides = _.sortBy(rides, function (o) {
+    return o.distance * -1
+  });
+
+  for (let STEP = 0; STEP < global.totalSteps; STEP++) {
+    setRides();
+    run();
+  }
+
+
+  function run() {
+    for (let auto of _.filter(autos, function (a) {
+      return a.ride > -1;
+    })) {
+      auto.step -= 1;
+      if (!auto.step) {
+        auto.finishedRides.push(auto.ride);
+        auto.ride = -1;
+      }
+    }
+  }
+
+  function setRides() {
+    for (let auto of _.filter(autos, {ride: -1})) {
+      if(sortedRides.length){
+        let autoRide = sortedRides.shift();
+        auto.ride = rides.indexOf(autoRide);
+        auto.steps = autoRide.distance;
+      }
+
+
+
+      /*for (let i in _.filter(rides, {inWay: 0})) {
+       let ride = rides[i];
+
+       let distance = getDistance(auto.x, auto.y, ride.xStart, ride.yStart);
+       if (distance === ride.startStep) {
+       rides[i].inWay = 1;
+       auto.ride = i;
+       }
+       }*/
+    }
+  }
 })();
+
+
+function getDistance(x1, y1, x2, y2) {
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+}
--- parser.js
+++ parser.js
@@ -3,7 +3,49 @@ const
     fs = require('fs'),
     q = require('q')
     ;
-
 (async() => {
-console.log("parser");
+  let names = ['a_example.in','b_should_be_easy.in','c_no_hurry.in','d_metropolis.in','e_high_bonus.in'];
+  names.map(parse)
+  function parse(name){
+    let content = fs.readFileSync('./source/' + name, 'ASCII')
+    var global, rides = [];
+
+
+    content = content.split('\n');
+    content.pop();
+
+    let first = content.shift().split(' ');
+
+    global = parseLine(first, {
+      rows: 0,
+      columns: 0,
+      vehicles: 0,
+      rides: 0,
+      bonus: 0,
+      totalSteps: 0
+
+    });
+    while (content.length) {
+      rides.push(parseLine(content.shift().split(' '), {
+        xStart: 0,
+        yStart: 0,
+        xFinish: 0,
+        yFinish: 0,
+        startStep: 0,
+        finishStep: 0,
+        inWay: 0
+      }))
+    }
+
+    fs.writeFileSync('./parsed/' + name.split('.')[0] + '.json', JSON.stringify({global, rides}));
+
+
+  }
+
+  function parseLine(array, output) {
+    for (let i in output) {
+      output[i] = Number(array.shift()) || 0;
+    }
+    return output;
+  }
 })();
